cb44a5aba70dbd55993a5b7a575c6fe5c8767001
Subramanian Murugan <sasubramanian_md@hotmail.com>
2021-06-30 14:59:32 +0530

    npm

--- nodeapps/src/index.js
+++ nodeapps/src/index.js
@@ -1,31 +1,3 @@
-const http = require('http');
-const { log } = console;
+const _ = require('lodash')
 
-const port =8080;
-
-const server = http.createServer((req, res) => {
-
-    let body='';
-    //attach data event on request object
-    req.on('data', chunk => {
-        body += chunk;
-        log(body);
-    });
-
-    //attach close event on response event
-    res.on('close', () => {
-        log('response close event is called')
-    });
-    res.on('finish', () => {
-        log('response has been sent /committed')
-    });
-    res.end("Hello,Node")
-});
-
-server.listen(port, () => {
-    console.log(`Http Server listens @ ${port}`)
-});
-
-server.on('request',(req,res)=>{
-   log(`request is recieved on ${port} -method is ${req.method} url ${req.url}`)
- })
+console.log(_.sum([1,2,3]))
